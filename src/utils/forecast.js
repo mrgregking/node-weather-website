3ca1e24b538100a8dd616e46801a5c221f1ab687
Greg King <mrgregking@msn.com>
2019-08-08 12:55:45 -0600

    Setup app for heroku

---
+++ src/utils/forecast.js
@@ -0,0 +1,29 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+  const url =
+    'https://api.darksky.net/forecast/6604bf34449a67dff64b8c0c3e66354e/' +
+    latitude +
+    ',' +
+    longitude
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback('Unable to connect to weather service!', undefined)
+    } else if (body.error) {
+      callback('Unable to find location', undefined)
+    } else {
+      callback(
+        undefined,
+        body.daily.data[0].summary +
+          ' It is currently ' +
+          body.currently.temperature +
+          ' degrees out. There is a ' +
+          body.currently.precipProbability +
+          '% chance of rain.'
+      )
+    }
+  })
+}
+
+module.exports = forecast
